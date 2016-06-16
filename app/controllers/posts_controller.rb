22cf6826287b797a9d22f64f1e7c6e4707fa8ac9
RamonSchmitt <ramon.schmitt@yahoo.com>
2016-06-16 12:07:40 +0200

    Authorize controller actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,10 +5,11 @@ class PostsController < ApplicationController
   end
 
   def create
-     post_params = params.require( :post ).permit( :content )
+    post_params = params.require( :post ).permit( :content )
 
-     @post = Post.new( content: post_params[:content] )
-     @post.user = current_user
+    @post = Post.new( content: post_params[:content] )
+    @post.user = current_user
+    authorize! :create, @post
 
      if @post.save
         redirect_to posts_path
@@ -19,16 +20,18 @@ class PostsController < ApplicationController
 
   def user
      @user = User.find( params[:user_id] )
+     authorize! :read, @user
 
      @posts = Post.where( user: @user ).order( created_at: :desc )
-  end
-
-  def user
-     @user = User.find( params[:user_id] )
-
-     @posts = Post.where( user: @user ).order( created_at: :desc )
+     authorize! :read, @posts
 
      @likes = @user.likes.joins( :post ).order( "posts.created_at DESC" )
+     authorize! :read, @likes
   end
-  
+
+  def index
+    @posts = Post.order( created_at: :desc )
+    authorize! :read, @posts
+  end
+
 end
